e35f11ec8f02cf16449faa5c3b0cd97415c1003e
Michael <109170801+michaelt244@users.noreply.github.com>
2026-09-16 00:25:47 -0700

    Add: find-k-closest-elements - submission-5

---
+++ Data Structures & Algorithms/find-k-closest-elements/submission-5.py
@@ -0,0 +1,42 @@
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+
+        left, right = 0, len(arr) - 1
+
+        result = []
+        
+        #using binary search find the postion where the target would be 
+        #this puts in cloest postion to the targer
+        while left < right:
+            middle = (left + right) // 2
+
+            if arr[middle] == x:
+                left = middle
+                break
+            elif arr[middle] < x:
+                left = middle + 1 
+            else:
+                right = middle - 1
+
+        l, r = left - 1, left
+
+        while len(result) < k:
+            if l < 0:
+                result.append(arr[r])
+                r += 1
+            elif r >= len(arr):
+                result.append(arr[l])
+                l -=1
+            elif abs(arr[l] - x) <= abs(arr[r] - x):
+                result.append(arr[l])
+                l -= 1
+            else:
+                result.append(arr[r])
+                r +=1
+
+        return sorted(result)
+
+
+        
+
+        
