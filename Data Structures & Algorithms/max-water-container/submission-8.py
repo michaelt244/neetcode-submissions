65b40d96e16dd0a0d246c7a55389e787f1a86273
Michael <109170801+michaelt244@users.noreply.github.com>
2026-09-19 00:18:08 -0700

    Add: max-water-container - submission-8

---
+++ Data Structures & Algorithms/max-water-container/submission-8.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+
+        #two pointers approach
+        left, right = 0, len(heights) - 1
+
+        area_max = 0
+
+        while left < right:
+            #lowest side
+            height = min(heights[left], heights[right])
+            #calculating the area between the two points
+            area = height * (right - left)
+            
+            #if the new area is bigger replace curent max
+            area_max = max(area_max, area)
+
+            #move the smaller height
+            if heights[left] < heights[right]:
+                left += 1
+            else:
+                right -=1
+            
+        return area_max
