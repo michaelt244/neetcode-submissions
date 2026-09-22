9344fc5e7f86cc2af331f60d8b66926055386977
Michael <109170801+michaelt244@users.noreply.github.com>
2026-09-21 23:03:05 -0700

    Add: longest-palindromic-substring - submission-7

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-7.py
@@ -0,0 +1,37 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+
+        if len(s) == 1:
+            return s
+        
+        longest_palindrome = ""
+        longest_len = 0
+
+        for i in range(len(s)):
+
+            #for odd palindromic, treat i as the center and expand outwards at the time
+
+            l, r = i, i 
+
+            while (0 <= l and r < len(s) and s[l] == s[r] ):
+                l -= 1
+                r += 1
+            odd_attempt = s[l + 1: r] 
+            odd_attempt_len = len(odd_attempt)
+
+            #for even Palindromic, treat i and i + 1 as the center so "bb" as the center
+            l, r = i, i + 1
+            while (0 <= l and r < len(s) and s[l] == s[r]):
+                l -= 1
+                r += 1
+            even_attempt = s[l + 1: r] 
+            even_attempt_len = len(even_attempt)
+
+            if even_attempt_len > longest_len:
+                longest_palindrome = even_attempt
+                longest_len = even_attempt_len
+            if odd_attempt_len > longest_len:
+                longest_palindrome = odd_attempt
+                longest_len = odd_attempt_len
+        
+        return longest_palindrome
