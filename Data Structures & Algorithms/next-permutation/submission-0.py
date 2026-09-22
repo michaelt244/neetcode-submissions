cf6e4082fd4d20d616837d0fd766991687c0253b
Michael <109170801+michaelt244@users.noreply.github.com>
2026-09-21 18:28:13 -0700

    Add: next-permutation - submission-0

---
+++ Data Structures & Algorithms/next-permutation/submission-0.py
@@ -0,0 +1,28 @@
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        right = len(nums) - 1
+        dip = -1
+        #finding the first dip in the list
+        while right  > 0:
+            if nums[right] > nums[right - 1]:
+                dip = right - 1
+                break
+            right -= 1
+        
+        #if not dip means were at the greatest permutation so just reverse the list
+        if dip == -1:
+            nums.reverse()
+            return
+
+        #finding the rightmost greatest value
+        right = len(nums) - 1
+        while right > dip:
+            if nums[right] > nums[dip]:
+                nums[dip], nums[right] = nums[right], nums[dip]
+                break
+            right -= 1
+        
+        return
